2e31a9f9ba6970e1860f7c0a60c43ad23cebce64
Maurizio Manca <maurizio.manca@swisscom.com>
2024-01-08 00:14:49 +0100

    Modified UI of homepage

--- src/components/HomeMenuItem.jsx
+++ src/components/HomeMenuItem.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 function HomeMenuItem({ menuItem }) {
     return (
         <Link to={menuItem.path}>
-            <div className='w-full bg-gray-100 rounded-xl p-4 h-96'>
-                <div className='justify-center mb-4 h-1/3'>
-                    <img src={menuItem.image} alt={menuItem.title} className='rounded-xl w-1/2' />
+            <div className='w-full bg-gray-100 rounded-xl p-4'>
+                <div className='p-4'>
+                    <img src={menuItem.image} alt={menuItem.title} className='rounded-xl object-fill' />
                 </div>
-                <div className='h-1/3'>
+                <div className=' text-left'>
                     <label className='text-xl font-bold'>{menuItem.title}</label>
                 </div>
-                <div className='h-1/3'>
-                    <label className='text-sm font-light'>{menuItem.description}</label>
+                <div className=''>
+                    <label className='text-base font-light'>{menuItem.description}</label>
                 </div>
             </div>
         </Link>
