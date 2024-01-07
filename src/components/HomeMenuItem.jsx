386bf66bc28b7920744452aa658fb826a89c8ae1
Maurizio Manca <maurizio.manca@swisscom.com>
2024-01-07 23:56:05 +0100

    changing ui of homepage

--- src/components/HomeMenuItem.jsx
+++ src/components/HomeMenuItem.jsx
@@ -5,13 +5,13 @@ function HomeMenuItem({ menuItem }) {
     return (
         <Link to={menuItem.path}>
             <div className='w-full bg-gray-100 rounded-xl p-4 h-96'>
-                <div className='justify-center mb-4 h-1/4'>
+                <div className='justify-center mb-4 h-1/3'>
                     <img src={menuItem.image} alt={menuItem.title} className='rounded-xl w-1/2' />
                 </div>
-                <div className='h-1/5'>
+                <div className='h-1/3'>
                     <label className='text-xl font-bold'>{menuItem.title}</label>
                 </div>
-                <div className='h-1/2'>
+                <div className='h-1/3'>
                     <label className='text-sm font-light'>{menuItem.description}</label>
                 </div>
             </div>
