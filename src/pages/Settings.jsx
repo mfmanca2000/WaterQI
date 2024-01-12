9148e74c45c93b77012246d74a6178ae4bd4745d
Maurizio Manca <maurizio.manca@swisscom.com>
2024-01-12 11:49:24 +0100

    Settings responsive UI

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -19,9 +19,9 @@ function Settings() {
             userId: userData?.$id,
             name: userData?.name,
             username: userData?.prefs.username,
-            email: userData?.email, 
+            email: userData?.email,
             showYourDataOnly: userData?.prefs.showYourDataOnly,
-            showMeasures: userData?.prefs.showStandaloneMeasures, 
+            showMeasures: userData?.prefs.showStandaloneMeasures,
             showMeasureGroups: userData?.prefs.showMeasureGroups,
         }
     })
@@ -32,9 +32,9 @@ function Settings() {
             userId: userData?.$id,
             name: userData?.name,
             username: userData?.prefs.username,
-            email: userData?.email, 
+            email: userData?.email,
             showYourDataOnly: userData?.prefs.showYourDataOnly,
-            showMeasures: userData?.prefs.showStandaloneMeasures, 
+            showMeasures: userData?.prefs.showStandaloneMeasures,
             showMeasureGroups: userData?.prefs.showMeasureGroups,
         });
 
@@ -44,12 +44,13 @@ function Settings() {
     const submit = async (data) => {
         console.log('UserData prefs: ' + JSON.stringify(userData.prefs))
         console.log('New username:' + getValues('username'))
-        const saved = await authService.savePreferences({ 
-            ...(userData.prefs), 
+        const saved = await authService.savePreferences({
+            ...(userData.prefs),
             showYourDataOnly: getValues('showYourDataOnly'),
             showStandaloneMeasures: getValues('showMeasures'),
-            showMeasureGroups: getValues('showMeasureGroups'), 
-            username: getValues('username') });
+            showMeasureGroups: getValues('showMeasureGroups'),
+            username: getValues('username')
+        });
         if (saved) {
             navigate('/');
         }
@@ -58,28 +59,46 @@ function Settings() {
 
     return (
         <div className='w-full p-8'>
-<label className='text-4xl'>Settings</label>
+            <label className='text-4xl'>Settings</label>
             <form onSubmit={handleSubmit(submit)} className="flex flex-wrap mt-4">
-                <div className='w-1/2 pr-4'>
-                    <div className='w-full'>                        
-                        <Input className='bg-gray-200 mb-4' label={t('settingsUserId')} disabled {...register('userId')} ></Input>
-                        <Input className='mb-4' label={t('settingsUsername')} {...register('username', { maxLength: 20 })}></Input>
-                        <Input className='mb-4' label={t('settingsName')} {...register('name', { required: true, maxLength: 50 })}></Input>
-                        <Input className='mb-4' label={t('settingsEmail')} {...register('email', { required: true, maxLength: 50 })}></Input>
+                <div className='w-full'>
+                    <div className='w-full'>
+                        <div className='mb-4 md:w-1/4 lg:w-1/4'>
+                            <Input className='bg-gray-200 ' label={t('settingsUserId')} disabled {...register('userId')} ></Input>
+                        </div>
+                        <div className='mb-4 md:w-1/4'>
+                            <Input className='mb-4' label={t('settingsUsername')} {...register('username', { maxLength: 20 })}></Input>
+                        </div>
+                        <div className='mb-4 md:w-1/4'>
+                            <Input className='mb-4' label={t('settingsName')} {...register('name', { required: true, maxLength: 50 })}></Input>
+                        </div>
+                        <div className='mb-4 md:w-1/4'>
+                            <Input className='mb-4' label={t('settingsEmail')} {...register('email', { required: true, maxLength: 50 })}></Input>
+                        </div>
+
+
+
                     </div>
                 </div>
-                <div className='w-1/2 pl-4'>
-                    <div className='w-full'>                                            
-                        <input type='checkbox' id='onlyYourMeasures' label={t('measuresShowYourDataOnly')} {...register('showYourDataOnly')}></input>
-                        <label className="mb-4 mr-4" htmlFor='onlyYourMeasures'>{t('measuresShowYourDataOnly')}</label>
-                        <input type='checkbox' id='showMeasures' label={t('measuresShowStandaloneMeasures')} {...register('showMeasures')}></input>
-                        <label className="mb-4 mr-4" htmlFor='showMeasures'>{t('measuresShowStandaloneMeasures')}</label>
-                        <input type='checkbox' id='showMeasureGroups' label={t('measuresShowMeasureGroups')} {...register('showMeasureGroups')}></input>
-                        <label className="mb-4 mr-4" htmlFor='showMeasureGroups'>{t('measuresShowMeasureGroups')}</label>
+                <div className='w-full'>
+                    <label className='text-xl'>Default settings for displaying measures</label>
+                    <div className='w-full flex flex-wrap'>
+                        <div className='p-2'>
+                            <input className='mr-2' type='checkbox' id='onlyYourMeasures' label={t('measuresShowYourDataOnly')} {...register('showYourDataOnly')}></input>
+                            <label className="mb-4" htmlFor='onlyYourMeasures'>{t('measuresShowYourDataOnly')}</label>
+                        </div>
+                        <div className='p-2'>
+                            <input className='mr-2' type='checkbox' id='showMeasures' label={t('measuresShowStandaloneMeasures')} {...register('showMeasures')}></input>
+                            <label htmlFor='showMeasures'>{t('measuresShowStandaloneMeasures')}</label>
+                        </div>
+                        <div className='p-2'>
+                            <input className='mr-2' type='checkbox' id='showMeasureGroups' label={t('measuresShowMeasureGroups')} {...register('showMeasureGroups')}></input>
+                            <label htmlFor='showMeasureGroups'>{t('measuresShowMeasureGroups')}</label>
+                        </div>
                     </div>
                 </div>
 
-                <Button type="submit" bgColor="bg-casaleggio-rgba" className='w-full mt-8 w-48'>
+                <Button type="submit" bgColor="bg-casaleggio-rgba" className='w-full mt-8'>
                     Save
                 </Button>
             </form >
